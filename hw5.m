e08364a874d87ef7af7f9daff5eb58bdf6210fc0
Simon <sapopecki@gmail.com>
2017-04-24 00:58:54 -0400

    problem 3 complete

--- hw5.m
+++ hw5.m
@@ -1,38 +1,44 @@
-%% Problem 1
+function hw5
+%% Problem 3
 %equation of motion:
 % J(thetaodd)+b(thetaod)+kthetao = kthetai
 global A B u
-
+u = 1; %u is a unit step input
 %made up moment of inertia, damping, and spring constant
 J = 1;
 b = 1;
-k = 10;
+k = 15;
 
 %define system in state space form (these are the A,B,C,D values from the
 %standard form equaition)
 A = [0 1; -k/J -b/J];
-B = [0 k/J];
+B = [0; k/J];
 C = [1 0];
 D = 0;
 
 %define simulation parameters
-x0 = [-1 0]; %these are the initial conditions (theta out, dtheta out)
+x0 = [0 -1]; %these are the initial conditions
 tstart = 0; %start time
-tstop = 10; %end time
-tspan = [tstart tstop];
+tstop = 15; %end time
+tspan = tstart:.01:tstop;
 
 %numerically integrate differential equation defined in function 'state2.m'
-[t,x] = ode23('state2',tspan,x0);
+[t,x] = ode23(@state2,tspan,x0);
 y = C*x' +D*u; %calculate output
+function thetadot = state2(t,x)
 
+%define differential equation to integrate
+thetadot = A*x + B*u;
+end
 %plot output
+figure(6)
 plot(t,y)
 title('System Output vs. Time, ODE23 Ogata B-3-13')
 xlabel('Time (s)')
 ylabel('Output (radians)')
 grid on
 
-
+end
 
 
 
